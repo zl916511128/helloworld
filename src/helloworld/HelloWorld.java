ce8ee577e9caf38bbcb581bc9de986cb1ff052af
Administrator <Administrator@windows10.microdone.cn>
2020-06-02 20:29:53 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World 随便一改");
+		System.out.println("Hello World 随便一改！");
 	}
 
 }
